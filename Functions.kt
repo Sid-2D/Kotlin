0691da576bf4828469e6871ea0e51e024e0305f1
Siddharth Chopra <sid6327@gmail.com>
2017-08-11 23:10:15 +0530

    Adding higher order fns.

--- Functions.kt
+++ Functions.kt
@@ -10,6 +10,18 @@ val greet = { greeting: String, name: String ->
 
 val fun1: (Int, Int) -> Int = { c, v -> Math.max(c, v) }
 
+val returnAnonymous = lambda@{ stopEarly: Boolean ->
+    println("line 1")
+    if (stopEarly) 
+    	return@lambda
+    println("line 2")
+}
+
+// Higher order fn
+val addNumToFun: (Int, (Int) -> Int) -> Int = { num, fn ->
+	num + fn(num)
+}
+
 fun main(args: Array<String>) {
     hello()
     hello.invoke()
@@ -21,4 +33,10 @@ fun main(args: Array<String>) {
     var sum = 0
     (1..10).forEach { sum += it }
     println(sum)
+
+	returnAnonymous(true)
+	returnAnonymous(false)
+
+	println(addNumToFun(9, fun(x): Int { return x + 1 }))
+	println(addNumToFun(9, { x: Int -> x + 1 }))
 }
